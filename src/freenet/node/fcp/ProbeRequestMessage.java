afb492854c8b6cbf5c7ee03b55e305bf55b2712a
Steve Dougherty <steve@asksteved.com>
2012-05-24 14:38:03 -0400

    FCP: negative hopsToLive is invalid.

--- src/freenet/node/fcp/ProbeRequestMessage.java
+++ src/freenet/node/fcp/ProbeRequestMessage.java
@@ -64,6 +64,8 @@ public class ProbeRequestMessage extends FCPMessage {
 			MHProbe.ProbeType type =  MHProbe.ProbeType.valueOf(fs.get(DMT.TYPE));
 			//If HTL is not defined default to MAX_HTL.
 			final short htl = fs.get(DMT.HTL) == null ? MHProbe.MAX_HTL : fs.getShort(DMT.HTL);
+			if (htl < 0) throw new MessageInvalidException(ProtocolErrorMessage.INVALID_MESSAGE,
+			                                               "hopsToLive cannot be negative.", null, false);
 			MHProbe.Listener listener = new MHProbe.Listener() {
 				@Override
 				public void onError(MHProbe.ProbeError error, String description) {
